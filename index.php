722a10aab30e1fc20ae71b2991122f09e39c894c
syayidalaziz10 <shorashiro15@gmail.com>
2023-11-22 15:17:45 +0700

    change tittle

--- index.php
+++ index.php
@@ -1,31 +1,31 @@
 <?php
-  include 'config.php';
+include 'config.php';
 
-  $getEmployees = "SELECT CONCAT(firstName, ' ', lastName) AS name, email, jobTitle FROM employees";
+$getEmployees = "SELECT CONCAT(firstName, ' ', lastName) AS name, email, jobTitle FROM employees";
+$employees = mysqli_query($config, $getEmployees);
+$total_data = mysqli_num_rows($employees);
+$limit = 10;
+$total_page = ceil($total_data / $limit);
+$page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+
+$start = ($page > 1) ? ($page * $limit) - $limit : 0;
+
+if (isset($_GET['query'])) {
+  $query = $_GET['query'];
+  $getEmployees = "SELECT CONCAT(firstName, ' ', lastName) AS name, email, jobTitle FROM employees WHERE firstName LIKE '%$query%' OR lastName LIKE '%$query%' OR email LIKE '%$query%' OR jobTitle LIKE '%$query%' LIMIT $start, $limit";
   $employees = mysqli_query($config, $getEmployees);
   $total_data = mysqli_num_rows($employees);
-  $limit = 10;
-  $total_page = ceil($total_data / $limit);
-  $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+} else {
+  $getEmployees = "SELECT CONCAT(firstName, ' ', lastName) AS name, email, jobTitle FROM employees LIMIT $start, $limit";
+  $employees = mysqli_query($config, $getEmployees);
+  $total_data = mysqli_num_rows($employees);
+}
 
-  $start = ($page > 1) ? ($page * $limit) - $limit : 0;
+$no = ($page > 1) ? ($page * $limit) - $limit + 1 : 1;
 
-  if (isset($_GET['query'])) {
-    $query = $_GET['query'];
-    $getEmployees = "SELECT CONCAT(firstName, ' ', lastName) AS name, email, jobTitle FROM employees WHERE firstName LIKE '%$query%' OR lastName LIKE '%$query%' OR email LIKE '%$query%' OR jobTitle LIKE '%$query%' LIMIT $start, $limit";
-    $employees = mysqli_query($config, $getEmployees);
-    $total_data = mysqli_num_rows($employees);
-  } else {
-    $getEmployees = "SELECT CONCAT(firstName, ' ', lastName) AS name, email, jobTitle FROM employees LIMIT $start, $limit";
-    $employees = mysqli_query($config, $getEmployees);
-    $total_data = mysqli_num_rows($employees);
-  }
-
-  $no = ($page > 1) ? ($page * $limit) - $limit + 1 : 1;
-
-  // $getEmployees = "SELECT CONCAT(firstName, ' ', lastName) AS name, email, jobTitle FROM employees LIMIT $start, $limit";
-  // $employees = mysqli_query($config, $getEmployees);
-  // print_r(mysqli_num_rows($employees));
+// $getEmployees = "SELECT CONCAT(firstName, ' ', lastName) AS name, email, jobTitle FROM employees LIMIT $start, $limit";
+// $employees = mysqli_query($config, $getEmployees);
+// print_r(mysqli_num_rows($employees));
 ?>
 
 <!doctype html>
@@ -34,9 +34,8 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Bootstrap demo</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <title>Customer | Filter Data</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
 <body>
@@ -61,13 +60,13 @@
             </tr>
           </thead>
           <tbody>
-            <?php foreach($employees as $employee) { ?>
-            <tr>
-              <th scope="row"><?php echo $no++ ?></th>
-              <td><?php echo $employee['name'] ?></td>
-              <td><?php echo $employee['email'] ?></td>
-              <td><?php echo $employee['jobTitle'] ?></td>
-            </tr>
+            <?php foreach ($employees as $employee) { ?>
+              <tr>
+                <th scope="row"><?php echo $no++ ?></th>
+                <td><?php echo $employee['name'] ?></td>
+                <td><?php echo $employee['email'] ?></td>
+                <td><?php echo $employee['jobTitle'] ?></td>
+              </tr>
             <?php } ?>
           </tbody>
         </table>
@@ -78,8 +77,8 @@
                 <span aria-hidden="true">&laquo;</span>
               </a>
             </li>
-            <?php for($i = 1; $i <= $total_page; $i++) { ?>
-            <li class="page-item"><a class="page-link" href="?page=<?= $i ?>"><?= $i ?></a></li>
+            <?php for ($i = 1; $i <= $total_page; $i++) { ?>
+              <li class="page-item"><a class="page-link" href="?page=<?= $i ?>"><?= $i ?></a></li>
             <?php } ?>
             <!-- <li class="page-item"><a class="page-link" href="?page=1">1</a></li>
             <li class="page-item"><a class="page-link" href="?page=2">2</a></li>
@@ -94,9 +93,7 @@
       </div>
     </div>
   </div>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
 </html>
